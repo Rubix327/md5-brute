6dfc5d31b5ce54628702dcf6745a1d09ddb3865a
Rubix327 <yarik.cool1@mail.ru>
2023-03-07 12:40:10 +0300

    v2

---
+++ README.md
@@ -0,0 +1,27 @@
+# Description
+This util helps to find the numeric code encoded by MD5 algorithm.
+
+<br /><br />
+# Tests
+Config:
+- CPU Ryzen 5600X (about 9% is used)
+- GPU GTX 1660 Super (about 7% is used)
+
+The tests:
+- 4-digits: 3.85s max - 2s avg
+- 5-digits: 4.13s max - 4s avg
+- 6-digits: 29.69s max - 29s avg
+
+<br /><br />
+# Versions
+
+## v2
+- Any digits-codes are available
+- Progress is written in percents
+- The code is significantly compressed
+
+## v1
+
+- Basic functionality
+- Only 4 and 6 digits are available
+- Measuring elapsed time
--- md5-brute.py
+++ md5-brute.py
@@ -3,81 +3,52 @@ import sys
 import time
 
 
-def found(s: str, start: float):
-    print("\nThe code is " + s)
-    end = time.time()
-    print("Elapsed time:", end - start, "s")
-    raise StopIteration
+def found(code: str, start_time: float):
+    end_time = time.time()
+    elapsed = end_time - start_time
+    print("\nThe code is " + code + " (found in " + f'{elapsed:.2f}' + "s)")
 
 
 def not_found():
     print("\nUnnable to find the code.")
 
 
-def six():
+def uni(d: int):
     start_time = time.time()
-    try:
-        search = input("Hashcode: ")
-        for a in range(10):
-            for b in range(10):
-                for c in range(10):
-                    for d in range(10):
-                        for e in range(10):
-                            for f in range(10):
-                                s = str(a) + str(b) + str(c) + str(d) + str(e) + str(f)
-                                hash = hashlib.md5(s.encode('utf-8')).hexdigest()
-                                sys.stdout.write("\rProgress: " + s + " - " + hash)
+    search = input("Hashcode: ")
+    max_n = 10 ** d
+    last_s = str(max_n - 1).zfill(d)
 
-                                if search == hash:
-                                    found(s, start_time)
+    for i in range(max_n):
+        code = str(i).zfill(d)
+        hsh = hashlib.md5(code.encode('utf-8')).hexdigest()
+        progress = "{:.2f}".format(i / (max_n - 1) * 100)
+        sys.stdout.write("\rProgress (" + progress + "%): " + code + " - " + hsh)
 
-                                if s == "999999":
-                                    not_found()
+        if search == hsh:
+            found(code, start_time)
+            break
 
-                                sys.stdout.flush()
-    except StopIteration:
-        pass
+        if code == last_s:
+            not_found()
 
-
-def four():
-    start_time = time.time()
-    try:
-        search = input("Hashcode: ")
-        for a in range(10):
-            for b in range(10):
-                for c in range(10):
-                    for d in range(10):
-                        s = str(a) + str(b) + str(c) + str(d)
-                        hash = hashlib.md5(s.encode('utf-8')).hexdigest()
-                        sys.stdout.write("\rProgress: " + s + " - " + hash)
-
-                        if search == hash:
-                            found(s, start_time)
-
-                        if s == "9999":
-                            not_found()
-
-                        sys.stdout.flush()
-    except StopIteration:
-        pass
+        sys.stdout.flush()
 
 
 def start():
     active = True
     while active:
-        s = input("Do you want to decode 4 or 6 digits code? ")
-        if s in ("q", "Q"):
+        amount = input("How many digits are in the code? ")
+        if amount in ("q", "Q"):
             active = False
             break
-        if s == "4":
+
+        if amount.isdigit():
+            uni(int(amount))
             active = False
-            four()
-        elif s == "6":
-            active = False
-            six()
         else:
             print("---")
-            print("Error: 4 and 6 are only available options.")
+            print("Error: only integer digits are available.")
             print("Type Q to exit.")
             print("---")
 
